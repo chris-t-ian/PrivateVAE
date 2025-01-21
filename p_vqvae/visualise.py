70ad86780695a3406cf1ddf56350e0ba3a2de4d8
Christian Schneider <schneider.chris.j@icloud.com>
2025-01-21 15:00:51 +0100

    adjusted handling of dimensions

--- p_vqvae/visualise.py
+++ p_vqvae/visualise.py
@@ -3,7 +3,7 @@ import os.path
 import matplotlib.pyplot as plt
 import numpy as np
 from torch import Tensor, get_device
-from p_vqvae.utils import calculate_AUC, get3d_middle_slice, check_and_remove_channel_dimension
+from p_vqvae.utils import calculate_AUC, get3d_middle_slice, check_and_remove_channel_dimension, select_tpr_at_low_fprs
 
 plt.rcParams['figure.figsize'] = [5, 3.75]
 
@@ -12,7 +12,7 @@ def plot_generated_images(img: np.array, n=3, file="data/plots/generated_images.
 
     img = check_and_remove_channel_dimension(img, dim)
     if n == 1:
-        image = get3d_middle_slice(img[0]) if dim == 3 else img[1]
+        image = get3d_middle_slice(img[0]) if dim == 3 else img[0]
         plt.imshow(image, cmap="gray")
 
     else:
@@ -20,10 +20,10 @@ def plot_generated_images(img: np.array, n=3, file="data/plots/generated_images.
         plt.style.use("default")
 
         for i in range(n):
-            image = get3d_middle_slice(img[i])
+            image = get3d_middle_slice(img[i]) if dim == 3 else img[i]
             ax[i].imshow(image, cmap="gray")
             ax[i].axis("off")
-            ax[i].title.set_text(f"Synthetic image {i}")
+            ax[i].title.set_text(f"Sample {i}")
 
     plt.savefig(file)
     plt.clf()
@@ -97,36 +97,46 @@ def plot_real_rec_syn(img: np.array, reconstructions: np.array, synthetic: np.ar
 
 
 def show_roc_curve(tprs, fprs, label=None, tprs2=None, fprs2=None, label2=None, tprs3=None, fprs3=None, label3=None,
-                   low_fprs=False, save=True, file_path=None, title: str = None):
+                   log_scale=False, save=True, file_path=None, title: str = None, low_fpr=0.001):
     auc = calculate_AUC(tprs, fprs)
+    tpr_at_low_fpr = select_tpr_at_low_fprs(tprs, fprs, low_fpr)
+    print("tpr at low fpr: ", tpr_at_low_fpr)
+
     if label:
-        plt.plot(fprs, tprs, label=f'{label} (AUC = {auc:.3f})')
+        _label = label if log_scale else label + f" AUC={auc:.3f}"
     else:
-        plt.plot(fprs, tprs, label=f'AUC = {auc:.3f}')
+        _label = f'TPR at {low_fpr:.3f} FPR = {tpr_at_low_fpr}' if log_scale else label + f'AUC = {auc:.3f}'
+    plt.plot(fprs, tprs, label=_label)
 
     if tprs2 is not None and fprs2 is not None:
         assert label and label2, "Specify labels."
         auc = calculate_AUC(tprs2, fprs2)
-        plt.plot(fprs2, tprs2, label=f'{label2} (AUC = {auc:.3f})')
+        #tpr_at_low_fpr = select_tpr_at_low_fprs(tprs2, fprs2, low_fpr)
+        _label2 = label2 if log_scale else label2 + f" AUC={auc:.3f}"
+        plt.plot(fprs2, tprs2, label=_label2)
 
     if tprs3 is not None and fprs3 is not None:
         assert label3, "Specify label 3"
         auc = calculate_AUC(tprs3, fprs3)
-        plt.plot(fprs3, tprs3, label=f'{label3} (AUC = {auc:.3f})')
+        tpr_at_low_fpr = select_tpr_at_low_fprs(tprs3, fprs3, low_fpr)
+        #tpr_at_low_fpr = "n/a" if tpr_at_low_fpr == 0.0 else tpr_at_low_fpr
+        #_label3 = label3 + f" TPR(FPR={low_fpr}) = {tpr_at_low_fpr}" if log_scale else label3 + f" AUC={auc:.3f}"
+        _label3 = label3 if log_scale else label3 + f" AUC={auc:.3f}"
+        plt.plot(fprs3, tprs3, label=_label3)
 
     plt.plot([0, 1], [0, 1], linestyle='--', color='gray')  # Diagonal line for random chance
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.subplots_adjust(left=0.2, bottom=0.2)  # avoid cutting off x-labels
     plt.legend(loc='lower right')
 
-    if low_fprs:
+    if log_scale:
         plt.yscale('log')
         plt.xscale('log')
         plt.xlim(1e-3, 1.0)
         plt.ylim(1e-3, 1.0)
 
-    plt.subplots_adjust(left=0.2, bottom=0.2)
-    file_path = f"data/plots/ROC_{label}_{label2}_{label3}_lowfprs{low_fprs}.png" if file_path is None else file_path
+    file_path = f"data/plots/ROC_{label}_{label2}_{label3}_log_scale{log_scale}.png" if file_path is None else file_path
     if title:
         plt.title(title)
     plt.grid()
@@ -152,11 +162,105 @@ def show_roc_curve_std(std1, std2=None, std3=None, file_path="data/plots", **kwa
     plt.savefig(file_path)
     plt.clf()
 
-def show_auc_tpr_plot(x_values, mean_auc, std_auc=None, xlabel=" ", ylabel="AUC"):
+def show_auc_tpr_plot(x_values, mean_auc, std_auc=None, xlabel=" ", ylabel="AUC", file_name=None):
     fig, ax = plt.subplots(nrows=1, ncols=1)
     fig.set_size_inches(6, 6)
-    ax.plot(x_values, mean_auc, yerr=std_auc, color="blue", ecolor='#888888')
+    if std_auc is None:
+        ax.plot(x_values, mean_auc, color="blue", marker="o")
+    else:
+        ax.errorbar(x_values, mean_auc, yerr=std_auc, color="blue", ecolor='#888888', marker="o")
     ax.set_ylabel(ylabel)
     ax.set_xlabel(xlabel)
-    plt.savefig(f"data/plots/{xlabel}_{ylabel}.png")
+    file_name = f"data/plots/{xlabel}_{ylabel}.png" if file_name is None else file_name
+    plt.savefig(file_name)
     plt.clf()
+
+
+def show_log_p_r(_adversary, title: str = None, file_path=None):
+    log_p_r_members, log_p_r_nonmembers = [], []
+    for it, b in enumerate(_adversary.true_memberships):
+        if b == 1:
+            log_p_r_members.append(_adversary.log_p_r[it])
+        else:
+            log_p_r_nonmembers.append(_adversary.log_p_r[it])
+
+    plt.hist(log_p_r_members, color='blue', label='Members', alpha=0.5, bins=50)
+    plt.hist(log_p_r_nonmembers, color='red', label='Non-members', alpha=0.5, bins=50)
+    plt.legend()
+    plt.xlabel("log p_R")
+    plt.ylabel("Frequency")
+    if title is not None:
+        plt.title(title)
+    plt.tight_layout()
+    file_path = f"data/plots/log_p_r_hist.png" if file_path is None else file_path
+    plt.savefig(file_path)
+    plt.clf()
+
+
+def plot_diffs(_adversary, title: str = None):
+    diffs_members = []
+    diffs_non_members = []
+    diffs = np.array(_adversary.diffs)
+    diffs = (diffs - diffs.min())/(diffs.max() - diffs.min())
+    for it, b in enumerate(_adversary.true_memberships):
+        if b == 1:
+            diffs_members.append(diffs[it])
+        else:
+            diffs_non_members.append(diffs[it])
+
+    plt.figure(figsize=(8, 5))
+    plt.hist(diffs_members, color='blue', label='Members', alpha=0.5, bins=75)
+    plt.hist(diffs_non_members, color='red', label='Non-members', alpha=0.5, bins=75)
+    plt.legend()
+    plt.xlabel("log p_S - log p_R")
+    if title is not None:
+        plt.title(title)
+    plt.tight_layout()
+    plt.savefig("data/plots/loss_differences_DOMIAS.png")
+    plt.clf()
+
+
+def plot_loss_distributions_domias(_adversary, combine=True):
+    log_p_s_members = []
+    log_p_r_members = []
+    log_p_s_nonmembers = []
+    log_p_r_nonmembers = []
+    for it, b in enumerate(_adversary.true_memberships):
+        if b == 1:
+            log_p_s_members.append(_adversary.log_p_s[it])
+            log_p_r_members.append(_adversary.log_p_r[it])
+        else:
+            log_p_s_nonmembers.append(_adversary.log_p_s[it])
+            log_p_r_nonmembers.append(_adversary.log_p_r[it])
+
+    fig, ax = plt.subplots(nrows=2, ncols=2)
+    fig.set_size_inches(16, 9)
+    ax[0, 0].hist(log_p_s_members, color='blue', label='syn loss members', alpha=0.5, bins=50)
+    ax[0, 0].hist(log_p_r_members, color='red', label='raw loss members', alpha=0.5, bins=50)
+    ax[0, 0].legend(loc="upper left")
+
+    ax[0, 1].hist(log_p_s_nonmembers, color='blue', label='syn loss non-members', alpha=0.5, bins=50)
+    ax[0, 1].hist(log_p_r_nonmembers, color='red', label='raw loss non-members', alpha=0.5, bins=50)
+    ax[0, 1].legend(loc="upper left")
+
+    ax[1, 0].hist(log_p_s_members, color='blue', label='syn loss members', alpha=0.5, bins=50)
+    ax[1, 0].hist(log_p_s_nonmembers, color='red', label='syn loss non members', alpha=0.5, bins=50)
+    ax[1, 0].legend(loc="upper right")
+
+    ax[1, 1].hist(log_p_r_members, color='blue', label='raw loss members', alpha=0.5, bins=50)
+    ax[1, 1].hist(log_p_r_nonmembers, color='red', label='raw loss nonmembers', alpha=0.5, bins=50)
+    ax[0, 1].legend(loc="upper right")
+    plt.xlabel("log p")
+    plt.tight_layout()
+    plt.savefig("data/plots/loss_distribution_DOMIAS.png")
+
+    if combine:
+        plt.figure(figsize=(12, 8))
+        plt.hist(log_p_s_members, color='blue', label='syn loss members', alpha=0.4, bins=85)
+        plt.hist(log_p_s_nonmembers, color='tab:cyan', label='syn loss non-members', alpha=0.4, bins=85)
+        plt.hist(log_p_r_members, color='tab:orange', label='raw loss members', alpha=0.4, bins=85)
+        plt.hist(log_p_r_nonmembers, color='tab:red', label='raw loss non-members', alpha=0.4, bins=85)
+        plt.legend()
+        plt.xlabel("log p")
+        plt.tight_layout()
+        plt.savefig("data/plots/loss_distribution_DOMIAS_combined.png")
